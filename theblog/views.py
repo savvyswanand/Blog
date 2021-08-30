aac944d635efd137d25f411edcd646d94ed073c6
savvyswanand <kulkarni.swanand@gmail.com>
2021-08-30 22:03:48 +0530

    add categories page

--- theblog/views.py
+++ theblog/views.py
@@ -13,6 +13,10 @@ class HomeView(ListView):
 	#ordering = ['-id']
 	ordering = ['-post_date']
 
+def CategoryView(request, cats):
+	category_posts = Post.objects.filter(category = cats)
+	return render(request, 'categories.html', {'cats': cats.title, 'category_posts':category_posts})
+
 class ArticleDetailView(DetailView):
 	model = Post
 	template_name = 'article_details.html'
